15d8715e080bd499c053b68d2b996ddfc920d6e8
GOKUL S <94166506+MASTERGOKUL@users.noreply.github.com>
2023-03-13 22:53:03 +0530

    Day 54 - 13/3/2023

---
+++ Day54/EliminationGame.java
@@ -0,0 +1,7 @@
+//problem : https://leetcode.com/problems/elimination-game/description/
+class Solution {
+    public int lastRemaining(int n) {
+        if(n==1) return 1;
+        return 2*(1 + n/2 - lastRemaining(n/2));
+    }
+}
