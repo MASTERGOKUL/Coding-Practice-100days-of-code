28d84e457cb3eb72bd7ea39a8fb6d9bc24bf61fa
GOKUL S <94166506+MASTERGOKUL@users.noreply.github.com>
2023-03-14 22:17:35 +0530

    Day 55  - 14/3/2023

---
+++ Day55/SqrtX.java
@@ -0,0 +1,10 @@
+//problem : https://leetcode.com/problems/sqrtx/description/
+class Solution {
+    public int mySqrt(int x) {
+  
+        if(x==0)
+            return x;
+        else
+            return (int) Math.pow(x,0.5);
+    }
+}
