614dcb0696e9c24d81e3d8ccab399431e3fc3dbf
GOKUL S <94166506+MASTERGOKUL@users.noreply.github.com>
2023-03-18 20:52:55 +0530

    Day 59 - 18/3/23

---
+++ Day59/FindtheIndexoftheFirstOccurrenceinaString.java
@@ -0,0 +1,20 @@
+//problem : https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int m = needle.length();
+        int n = haystack.length();
+
+        for (int windowStart = 0; windowStart <= n - m; windowStart++) {
+            for (int i = 0; i < m; i++) {
+                if (needle.charAt(i) != haystack.charAt(windowStart + i)) {
+                    break;
+                }
+                if (i == m - 1) {
+                    return windowStart;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
