2cf494f099e234f13783b0338704cdbf671d86c9
GOKUL S <94166506+MASTERGOKUL@users.noreply.github.com>
2023-03-26 10:26:47 +0530

    Day 67 - 26/3/23

---
+++ Day67/DecimalToBinary.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    System.out.println(Integer.toBinaryString(n));
+    }
+}
---
+++ Day67/MyFirst1stContest.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println((n-a)+" "+(n-a-b));
+    }
+}
