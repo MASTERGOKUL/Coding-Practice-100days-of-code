9067019f3a571837f448e6cc6530016f996bc1ca
GOKUL S <94166506+MASTERGOKUL@users.noreply.github.com>
2023-04-07 20:24:16 +0530

    Day 79 - 7/4/2023

---
+++ Day79/countDistinctNumbersOnBoard.java
@@ -0,0 +1,7 @@
+//problem : https://leetcode.com/problems/count-distinct-numbers-on-board/
+class Solution {
+    public int distinctIntegers(int n) {
+        if(n==1) return n;
+        return n-1;
+    }
+}
