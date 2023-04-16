d5e92cf82452d402642c3a849b3babf632b9d21e
GOKUL S <94166506+MASTERGOKUL@users.noreply.github.com>
2023-04-16 21:38:35 +0530

    Day 88 - 16/4/23

---
+++ Day88/SumofDigitsofStringAfterConvert.java
@@ -0,0 +1,16 @@
+//problem : https://leetcode.com/problems/sum-of-digits-of-string-after-convert/
+class Solution {
+    public int getLucky(String s, int k) {
+        String n = "";
+        int num = 0;
+        for(int i=0; i<s.length(); i++) n += s.charAt(i)-'a'+1 + "";
+        while(k-- > 0){
+            num = 0;
+            for(char c: n.toCharArray()){
+                num += Integer.parseInt(c + "");
+            }
+            n = String.valueOf(num);
+        }
+        return num;
+    }
+}
