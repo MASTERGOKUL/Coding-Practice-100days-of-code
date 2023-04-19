7cbdab4fd9bba81aab60fbdc09163fef81bcec2f
GOKUL S <94166506+MASTERGOKUL@users.noreply.github.com>
2023-04-19 22:57:25 +0530

    Day 91 - 19/4/23

---
+++ Day91/NumberofSegmentsisaString.java
@@ -0,0 +1,14 @@
+//problem : https://leetcode.com/problems/number-of-segments-in-a-string/description/
+class Solution {
+    public int countSegments(String s) {
+        int segmentCount = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if ((i == 0 || s.charAt(i-1) == ' ') && s.charAt(i) != ' ') {
+                segmentCount++;
+            }
+        }
+
+        return segmentCount;
+    }
+}
