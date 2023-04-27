a2a191292e09845b1ae776acd7d99369bf734f38
GOKUL S <94166506+MASTERGOKUL@users.noreply.github.com>
2023-04-27 21:04:41 +0530

    Day 99 - 27/4/2023

---
+++ Day99/ConstructtheRectangle.java
@@ -0,0 +1,11 @@
+//problem : https://leetcode.com/problems/construct-the-rectangle/description/
+class Solution {
+  public int[] constructRectangle(int area) {
+    int width = (int) Math.sqrt(area);
+
+    while (area % width > 0)
+      --width;
+
+    return new int[] {area / width, width};
+  }
+}
